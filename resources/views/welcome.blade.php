4d4c78f16c2cc4f9eb1db3b3dd1987d8e96b075f
ptumwebaze <ptersont@gmail.com>
2023-08-10 11:54:23 +0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,392 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="TemplateMo">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <title>Education Meeting HTML5 Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/templatemo-edu-meeting.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/lightbox.css') }}">
+<!--
+
+TemplateMo 569 Edu Meeting
+
+https://templatemo.com/tm-569-edu-meeting
+
+-->
+  </head>
+
+<body>
+
+  <!-- Sub Header -->
+  <div class="sub-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-sm-8">
+          <div class="left-content">
+            <p><i class="fa fa-phone"></i> +971-52377-8795 &#160;&#160;&#160;&#160;&#160;&#160;<i class="fa fa-envelope"></i> management@exclusiveholidayhomes.ae</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-4">
+          <div class="right-icons">
+            <ul>
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+              <li><a href="#"><i class="fa fa-whatsapp"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                  <nav class="main-nav">
+                      <!-- ***** Logo Start ***** -->
+                      <a href="index.html" class="logo">
+                          EXCLUSIVE HOLIDAY HOMES
+                      </a>
+                      <!-- ***** Logo End ***** -->
+                      <!-- ***** Menu Start ***** -->
+                      <ul class="nav">
+                        <li class="has-sub">
+                            <a href="javascript:void(0)">Dubai</a>
+                            <ul class="sub-menu">
+                                <li><a href="meetings.html">Upcoming Meetings</a></li>
+                                <li><a href="meeting-details.html">Meeting Details</a></li>
+                            </ul>
+                        </li>
+                          <li class="has-sub">
+                              <a href="javascript:void(0)">Bodrum</a>
+                              <ul class="sub-menu">
+                                  <li><a href="meetings.html">Upcoming Meetings</a></li>
+                                  <li><a href="meeting-details.html">Meeting Details</a></li>
+                              </ul>
+                          </li>
+                          <li class="scroll-to-section"><a href="#courses">About</a></li>
+                          <li class="scroll-to-section"><a href="#contact">Contact</a></li>
+                      </ul>
+                      <a class='menu-trigger'>
+                          <span>Menu</span>
+                      </a>
+                      <!-- ***** Menu End ***** -->
+                  </nav>
+              </div>
+          </div>
+      </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <!-- ***** Main Banner Area Start ***** -->
+  <section class="section main-banner" id="top" data-section="section1">
+      <video autoplay muted loop id="bg-video">
+          <source src="{{ asset('assets/images/course-video.mp4') }}" type="video/mp4" />
+      </video>
+
+      <div class="video-overlay header-text">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="caption text-center">
+                <h1 class="text-center text-white">EXCLUSIVE HOLIDAY HOMES</h1>
+          </div>
+              </div>
+            </div>
+          </div>
+      </div>
+  </section>
+  <!-- ***** Main Banner Area End ***** -->
+
+  <section class="services">
+    <div class="container">
+      <div class="row px-3">
+        <div class="col-md-5 ms-5 item" style="background-image: url('{{ asset('assets/images/dubai4.jpg') }}'); height: 300px;">
+            <h2 style="margin-top: 80px">Dubai</h2>
+        </div>
+        <div class="col-md-5 ms-5 item" style="background-image: url('{{ asset('assets/images/dubai3.jpg') }}'); height: 300px;">
+            <h2 style="margin-top: 80px">Bodrum</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="upcoming-meetings" id="meetings">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="section-heading">
+            <h2>Your Dream Holiday Home</h2>
+          </div>
+        </div>
+        <p class="text-white">Hi, we are Exclusive Holiday Homes, a licensed short term rental operator based in Dubai. As our name suggests, our properties are exclusive, and truly best in class.
+          We maintain our properties with no expense spared, and look after our guests with their every request.
+          The result is the happiest customers who stay with us time and time again. We provide ongoing support and butler service throughout your stay,
+          effectively filling the gap between luxury hotels and holiday homes.</p>
+      </div>
+    </div>
+    <section class="our-courses" id="courses">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="section-heading">
+                <h2 class="text-center">Clients Reviews</h2>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="owl-courses-item owl-carousel">
+                <div class="item">
+                  <div class="down-content px-2">
+                    <div class="text-right"><p>Michael<br>
+                    2022-02-09 <i class="fa fa-bell pull-right"></i></p></div>
+                    <div class="info">
+                      <div class="row">
+                        <div class="col-12">
+                          <ul>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-check-circle"></i></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <p>Been to dubai 7 times and this was by far the best place that i have stayed.</p>
+                  </div>
+                </div>
+                <div class="item">
+                    <div class="down-content px-2">
+                      <div class="text-right"><p>Miata<br>
+                        2021-08-13 <i class="fa fa-bell pull-right"></i></p></div>
+                      <div class="info">
+                        <div class="row">
+                          <div class="col-8">
+                            <ul>
+                              <li><i class="fa fa-star"></i></li>
+                              <li><i class="fa fa-star"></i></li>
+                              <li><i class="fa fa-star"></i></li>
+                              <li><i class="fa fa-star"></i></li>
+                              <li><i class="fa fa-check-circle"></i></li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                      <p>The villa is absolutely great. All of the pictures are real.
+                        The butler Chami will make your stay there perfect.</p>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="our-courses" id="courses">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="section-heading">
+                <h2 class="text-center">Featured Homes</h2>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="owl-courses-item owl-carousel">
+                <div class="item">
+                  <img src="{{ asset('assets/images/dubai4.jpg') }}" alt="">
+                  <div class="down-content">
+                    <h4>Luxury Living & Panoramic views from St Regis</h4>
+                    <p class="px-2 py-2">Dubai, United Arab Emirates</p>
+                  </div>
+                </div>
+                <div class="item">
+                  <img src="{{ asset('assets/images/dubai3.jpg') }}" alt="">
+                  <div class="down-content">
+                    <h4>Five Palm Beach Villa with Private Pool</h4>
+                    <p class="px-2 py-2">Dubai, United Arab Emirates</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+  </section>
+
+
+  <section class="contact-us" id="contact">
+    <div class="container">
+      <div class="row">
+        <h2 class="text-white mb-5" style="margin-left: 400px;">Get In Touch With Us</h2>
+        <div class="col-lg-6">
+          <img src="{{ asset('assets/images/course-03.jpg') }}" alt="">
+        </div>
+        <div class="col-lg-6 align-self-center">
+          <div class="row">
+            <div class="col-lg-12">
+              <form id="contact" action="" method="post">
+                <div class="row">
+                  <div class="col-lg-12">
+                  </div>
+                    <fieldset>
+                      <input name="name" type="text" id="name" placeholder="Name" required="">
+                    </fieldset>
+                    <fieldset>
+                      <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*" placeholder="Email" required="">
+                    </fieldset>
+                    <fieldset>
+                      <input name="subject" type="text" id="subject" placeholder="PHONE" required="">
+                    </fieldset>
+                    <fieldset>
+                      <textarea name="message" type="text" class="form-control" id="message" placeholder="Message" required=""></textarea>
+                    </fieldset>
+                    <fieldset>
+                      <button type="submit" id="form-submit" class="button btn-warning">SEND</button>
+                    </fieldset>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <section class="our-courses" id="courses">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="section-heading">
+                <h2 class="text-center">Awards Nominees</h2>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="owl-courses-item owl-carousel">
+                <div class="item">
+                  <img src="https://exclusiveholidayhomes.ae/wp-content/uploads/2022/09/airbnb-superhost-badge-637x264-1.jpg" alt="" height="250px">
+                </div>
+                <div class="item">
+                    <img src="https://exclusiveholidayhomes.ae/wp-content/uploads/2022/09/Digital-Award-TRA-2022.png" alt="">
+                </div>
+                <div class="item">
+                    <img src="https://exclusiveholidayhomes.ae/wp-content/uploads/2022/09/Digital-Award-TRA-2022-1.png" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    <div class="footer">
+      <div class="row" style="text-align: left">
+        <div class="col-md-4 px-5">
+            <img src="{{ asset('assets/images/logo.png') }}" alt="">
+        </div>
+        <div class="col-md-4">
+          <h4 class="text-white mb-4" style="text-transform: uppercase;">Dubai</h4>
+          <p style="text-transform: capitalize;">Five Palm Beach Villa with Private Pool
+            Luxury Living & Panoramic views from St Regis
+            Five Palm Luxury Beach Villa with Private Pool
+            St Regis Living and Panoramic Views – High floor
+            Luxury Living & views at St Regis – High Floor
+            Stunning Palm Villa with Rooftop Garden and Pool
+            Ultimate 5BR Duplex Penthouse – Private Pool</p>
+        </div>
+        <div class="col-md-4">
+          <h4 class="text-white mb-4" style="text-transform: uppercase;">Bodrum</h4>
+          <p style="text-transform: capitalize;">
+            Mandarin Oriental Bodrum Villa<br>
+            Mandarin Oriental Bodrum Villa
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+        <div class="row">
+            <div class="col-md-6"><p>© 2022 Exclusive Holiday Homes. All Right Reserved.</p></div>
+            <div class="col-md-6">
+                <div class="right-icons pull-center">
+                    <a href="#"><i class="fa fa-facebook px-2"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-whatsapp px-2"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+  </section>
+
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <script src="{{ asset('assets/js/isotope.min.js') }}"></script>
+    <script src="{{ asset('assets/js/owl-carousel.js') }}"></script>
+    <script src="{{ asset('assets/js/lightbox.js') }}"></script>
+    <script src="{{ asset('assets/js/tabs.js') }}"></script>
+    <script src="{{ asset('assets/js/video.js') }}"></script>
+    <script src="{{ asset('assets/js/slick-slider.js') }}"></script>
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
+    <script>
+        //according to loftblog tut
+        $('.nav li:first').addClass('active');
+
+        var showSection = function showSection(section, isAnimate) {
+          var
+          direction = section.replace(/#/, ''),
+          reqSection = $('.section').filter('[data-section="' + direction + '"]'),
+          reqSectionPos = reqSection.offset().top - 0;
+
+          if (isAnimate) {
+            $('body, html').animate({
+              scrollTop: reqSectionPos },
+            800);
+          } else {
+            $('body, html').scrollTop(reqSectionPos);
+          }
+
+        };
+
+        var checkSection = function checkSection() {
+          $('.section').each(function () {
+            var
+            $this = $(this),
+            topEdge = $this.offset().top - 80,
+            bottomEdge = topEdge + $this.height(),
+            wScroll = $(window).scrollTop();
+            if (topEdge < wScroll && bottomEdge > wScroll) {
+              var
+              currentId = $this.data('section'),
+              reqLink = $('a').filter('[href*=\\#' + currentId + ']');
+              reqLink.closest('li').addClass('active').
+              siblings().removeClass('active');
+            }
+          });
+        };
+
+        $('.main-menu, .responsive-menu, .scroll-to-section').on('click', 'a', function (e) {
+          e.preventDefault();
+          showSection($(this).attr('href'), true);
+        });
+
+        $(window).scroll(function () {
+          checkSection();
+        });
+    </script>
+</body>
+
+</body>
+</html>
